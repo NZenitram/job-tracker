d4e965f24c48e278509c13c07eadb2faf4859917
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-10-20 15:58:49 -0600

    Updated the jobs view and controller with update and delete functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root :to => 'companies#index'
 
   resources :companies do
     resources :jobs
+
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
